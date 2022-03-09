c2840421014c194fe706c591adcf5d161bcc7b92
JeanBauch <jean.bauch@bomjesus.br>
2022-03-09 09:07:42 -0300

    Projeto criado

---
+++ backend/src/main/java/com/jeanbauch/jbmovie/JbmovieApplication.java
@@ -0,0 +1,13 @@
+package com.jeanbauch.jbmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeanbauch/jbmovie/JbmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbauch.jbmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
